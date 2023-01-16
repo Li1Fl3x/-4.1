253a67190e4c32f8ec56b9266174a858b86a8441
Admin <Admin@DESKTOP-C49GG1B>
2023-01-16 17:41:16 +0200

    Добавьте файлы проекта.

---
+++ работа 4.1/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = new int[25];
+int i = new Random();
+if (i > 0);
+{
+    Console.WriteLine("i" + " " + 7);
+}
+if (i == 0);
+{
+    Console.WriteLine(0);
+}
+if (i < 0);
+{
+    Console.WriteLine("i * i");
+}
